e000e0fdf25cfad20f8dfe0ae9a5569e9439e3cd
ruben <rubenrara@gmail.com>
2018-05-14 12:27:31 +0200

    bug fix

--- src/GreyscaleImage.hpp
+++ src/GreyscaleImage.hpp
@@ -1,13 +1,14 @@
 #ifndef GREYSCALEIMAGE_HPP
-#define GREYSCALEPIXEL_HPP
+#define GREYSCALEImage_HPP
 #include "Image.hpp"
 #include "GreyscalePixel.hpp"
+#include "vector-2d.hpp"
 class GreyscaleImage :public Image
 {
 private:
 	GreyscalePixel pixels[640][480];
 public:
-	void setPixel(Vector2D position, ColorPixel value);
+	void setPixel(Vector2D position, GreyscalePixel value);
 	GreyscalePixel getPixel(Vector2D position);
 };
 #endif // GREYSCALEIMAGE_HPP
