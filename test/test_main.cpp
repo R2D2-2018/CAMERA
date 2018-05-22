b9321cb6ed82322b8b046cea0a5030f326ea176c
ruben <rubenrara@gmail.com>
2018-05-22 12:57:00 +0200

    removed test_ image and placed code in test main  Please enter the commit message for your changes. Lines starting

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,10 +1,12 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "../src/vector_2d.cpp"
-
 #include "../src/mock.cpp"
-
 #include "../src/camera.cpp"
-
+#include "../src/greyscale_pixel.cpp"
+#include "../src/vector_2d.cpp"
+#include "../src/color_image.cpp"
+#include "../src/color_pixel.cpp"
+#include "../src/greyscale_image.cpp"
 #include "../src/greyscale_pixel.cpp"
 
 #include "catch.hpp"
@@ -100,4 +102,34 @@ TEST_CASE("Camera mock set negative values") {
 
     REQUIRE(camera.getResolution() == resolution);
     REQUIRE(camera.getFPS() == -30);
+}
+
+TEST_CASE( "testing colorImage" ) {
+    ColorImage kleurplaatje;
+    ColorPixel redPixel;
+    redPixel.setRed(127);
+    Vector2D coordinate;
+    coordinate.setX(120);
+    coordinate.setY(125);
+    kleurplaatje.setPixel(coordinate, redPixel);
+    ColorPixel testPixel;
+    testPixel.setRed(127);
+    REQUIRE(kleurplaatje.getPixel(2,2) == testPixel);
+    testPixel.setGreen(200);
+    REQUIRE_FALSE(kleurplaatje.getPixel(coordinate) == testPixel);
+}
+
+TEST_CASE( "testing colorImage" ) {
+    ColorImage image;
+    GreyscalePixel pixel;
+    pixel.set(95);
+    Vector2D coordinate;
+    coordinate.setX(120);
+    coordinate.setY(125);
+    kleurplaatje.setPixel(pixel);
+    pixel testPixel;
+    testPixel.set(95);
+    REQUIRE(image.getPixel(2,2) == testPixel);
+    testPixel.set(200);
+    REQUIRE_FALSE(kleurplaatje.getPixel(coordinate) == testPixel);
 }
