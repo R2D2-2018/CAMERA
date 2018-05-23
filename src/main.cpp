af57e1248834cd3e4bfd0af172d4a0d44e40fcc9
ruben <rubenrara@gmail.com>
2018-05-23 03:26:46 +0200

    made for loop to decrompress the ycbcr422 fomrat

--- src/main.cpp
+++ src/main.cpp
@@ -30,5 +30,32 @@ int main() {
 
     hwlib::cout << image.getPixel(position).getPixel() << hwlib::endl;
 
+    int cb[3];
+    int cr[3];
+    int y[3];
+    int counter = 0;
+    int const sensordataSize = 8;
+                                   //[cb0 y0 cr0 y1 cb2 y2 cr2 y3 ]
+    int sensorData[sensordataSize] = {23, 7, 9,  3,  5, 12, 23, 42};
+    int saved_images = 0;
+    int i = 1;
+    int iy = 0;
+    while(saved_images < 4){
+            //std::cout << i;
+            cb[counter] = sensorData[i-1];
+            y[counter] = sensorData[i + iy];
+            cr[counter] = sensorData[i+1];
+            counter++;
+            saved_images++; 
+            if (iy == 2) {
+                iy = 0;
+                i += 4;
+            } else{
+                iy = 2;
+            }
+            }
+    hwlib::cout << cb[0] << " " << cb[1] << " " << cb[3] << hwlib::endl;
+    hwlib::cout << cr[0] << " " << cr[1] << " " << cr[3]<< hwlib::endl;
+    hwlib::cout << y[0] << " " << y[1] << " " << y[3]<< hwlib::endl;
     return 0;
 }
