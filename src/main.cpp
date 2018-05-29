0f664a892da1e023697d1e964e60d72e746daf6b
Jari van Dam <jari@jarivandam.nl>
2018-05-29 13:04:37 +0200

    Cleanup of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,10 @@ int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
 
     hwlib::wait_ms(1000);
-    Vector2D resolution;
-    resolution.setX(100);
-    resolution.setY(150);
     Mock camera;
-    camera.setFPS(5);
-    camera.setResolution(resolution);
-
-    camera.setFPS(421);
-
     Vector2D position(5, 10);
+    Image<GreyscalePixel> plaatje = camera.takeGreyImage();
 
-    Image<YCbCr> plaatje;
-
-    GreyscalePixel zwartpixel;
-    YCbCr kleurplaatje = plaatje.getPixel(position);
-    zwartpixel = kleurplaatje;
-
-    Image<GreyscalePixel> zwartplaatje;
-    hwlib::cout << "Image:" << hwlib::endl;
-    hwlib::cout << zwartplaatje << hwlib::endl;
+    hwlib::cout << plaatje << hwlib::endl;
     return 0;
 }
