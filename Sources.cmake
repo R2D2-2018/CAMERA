d4c088663efc450a40d08a8487a9a1cd65805f9b
Willem de Groot <willembike@gmail.com>
2018-06-26 13:29:40 +0200

    Updated build environment files

--- Sources.cmake
+++ Sources.cmake
@@ -11,8 +11,7 @@ include_directories (${catch}/single_include)
 # Source Files:
 
 set (sources
-    src/pi_cam.cpp
     src/vector_2d.cpp
-    src/base64_encoder.cpp	
-    src/main.cpp
+    src/pi_cam.cpp
+    src/base64_encoder.cpp
 )
